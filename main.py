e9baa4c3119dcc44039d43438dba8e1bb618e260
knathanwilson <knathanwilson@gmail.com>
2017-10-31 10:27:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask import Flask, request, redirect, Response, make_response, url_for
 
 app = flask(__name__)
 
-
 @app.route('/')
-def index(): return 'this is working???'
+def index():return 'this is working???'
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
